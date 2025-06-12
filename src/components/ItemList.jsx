6efb7e4c173e1611021a4d101602b31121e1f65c
Vladimir Vaisov <vaisov.vladimir@gmail.com>
2025-06-12 14:38:20 +0300

    Add sorting functionality to ItemList component
    - Introduced a sorting dropdown using react-select to allow users to sort items by packed and unpacked status.
    - Implemented sorting logic to rearrange items based on the selected option.
    - Updated the ItemList component to display the sorting dropdown when items are present.
    - Removed unused CSS styles related to the item list section.

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,42 @@
+import { useState } from 'react';
 import EmptyView from './EmptyView';
+import Select from 'react-select';
+
+const sortingOptions = [
+  { value: 'default', label: 'Sort by default' },
+  { value: 'packed', label: 'Sort by packed' },
+  { value: 'unpacked', label: 'Sort by unpacked' },
+];
 
 export default function ItemList({
   items,
   handleDeleteById,
   handleToggleItemPacked,
 }) {
+  const [sortBy, setSortBy] = useState('default');
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === 'packed') {
+      return b.packed - a.packed; // Sort packed items first
+    } else if (sortBy === 'unpacked') {
+      return a.packed - b.packed; // Sort unpacked items first
+    }
+    return;
+  });
+
   return (
     <ul className='item-list'>
       {items.length === 0 && <EmptyView />}
-      {items.map((item) => (
+      {items.length > 0 && (
+        <section className='sorting'>
+          <Select
+            defaultValue={sortingOptions[0]}
+            onChange={(option) => setSortBy(option.value)}
+            options={sortingOptions}
+          />
+        </section>
+      )}
+      {sortedItems.map((item) => (
         <Item
           onToggleItem={handleToggleItemPacked}
           onDeleteItem={handleDeleteById}
